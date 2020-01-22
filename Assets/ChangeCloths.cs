fbe4921371d71ab0f68086652caf05d8a9ac5afe
Allen Cass <acass@crossmedia411.com>
2020-01-22 08:20:13 -0800

    Added init files

---
+++ Assets/ChangeCloths.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class ChangeCloths : MonoBehaviour {
+    
+    public Renderer mShirt, mPants;
+    public Texture[] mShirtTextures;
+
+    public void changeMyShirt(int myTexture) {
+
+        switch (myTexture) {
+            case 0:
+                mShirt.material.SetTexture("_MainTex", mShirtTextures[0]);
+                mPants.material.SetTexture("_MainTex", mShirtTextures[0]);
+                break;
+            case 1:
+                mShirt.material.SetTexture("_MainTex", mShirtTextures[1]);
+                mPants.material.SetTexture("_MainTex", mShirtTextures[0]);
+                break;
+            case 2:
+                mShirt.material.SetTexture("_MainTex", mShirtTextures[2]);
+                mPants.material.SetTexture("_MainTex", mShirtTextures[0]);
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ Assets/MainController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class MainController : MonoBehaviour {
+
+    public GameObject mTrigger, mBackTrigger;
+
+    void OnTriggerEnter(Collider other) {
+
+        if (other.CompareTag("front"))
+            mTrigger.SetActive(true);
+            Debug.Log("Front");
+               
+        if (other.CompareTag("back"))
+            mBackTrigger.SetActive(true);
+            Debug.Log("Back");
+    }
+
+    void OnTriggerExit(Collider other) {
+
+        if (other.CompareTag("front"))
+            mTrigger.SetActive(false);
+
+        if (other.CompareTag("back"))
+            mBackTrigger.SetActive(false);
+    }
+
+}
